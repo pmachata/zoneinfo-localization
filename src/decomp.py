92b80a0d16aa046918d78fb3edda7fff2312f64e
Petr Machata <pmachata@redhat.com>
2010-12-10 22:15:36 +0100

    Try harder to find the right translation of tzid prefix
    - po/messages contain import script messages.  The stuff that's there
      should be reviewed manually for correctness.  In cases listed there,
      the script was unsure which translation to pick, and ended up taking
      the one with more uses (or simply one of them, in 1:1 cases).

--- src/decomp.py
+++ src/decomp.py
@@ -15,18 +15,27 @@ def extract_context (tzid, str_comps, top_context, message=None):
     if len (str_comps) != len (tzid.components):
         return
 
+    translation = str_comps[-1]
     if message is None:
-        n = tzid.name ()
-        has = n in new_messages
-        message = new_messages.setdefault (n, polib.POEntry ())
-        if not message.comment:
-            message.comment = "Prefix of"
-        message.comment = message.comment + " " + top_context.name ()
-        if has:
+        name = tzid.name ()
+
+        # Pair (comment, dict of candidate translations)
+        candidates_pair = new_messages.setdefault (name,
+                                                   ["Prefix of", {}])
+        (_, candidates) = candidates_pair
+
+        # Pair (use counter, translation)
+        candidate = candidates.setdefault (translation,
+                                           [0, polib.POEntry ()])
+        (_, message) = candidate
+
+        candidates_pair[0] = candidates_pair[0] + " " + top_context.name ()
+        candidate[0] += 1
+        if candidate[0] > 1: # already seen
             return
 
     message.msgid = tzid.components[-1]
-    message.msgstr = str_comps[-1]
+    message.msgstr = translation
     if len (tzid.components) > 1:
         message.msgctxt = "/".join (tzid.components[:-1])
         extract_context (TZID (message.msgctxt), str_comps[:-1], top_context)
@@ -41,6 +50,68 @@ for message in po:
     if len (tzid.components) > 1:
         extract_context (tzid, message.msgstr.split ("/"), tzid, message)
 
-added_messages = list (new_messages[msgid] for msgid in sorted (new_messages))
+def filter_candidates (candidates):
+    # Here we try to got rid of obvious mistakes.
+
+    # Look for the strongest candidate.  Sorting is done by number of
+    # uses.  In case of tie, the one without underscores wins.
+    candidate_list = list ((list (item) for item in candidates.items ()))
+    def candidate_sorting_key ((translation,(uses,message))):
+        return (uses, "_" not in translation)
+    candidate_list.sort (key=candidate_sorting_key, reverse=True)
+    if len (candidate_list) == 1:
+        return candidate_list
+
+    # Normalize use of " " and "_".  Pick whatever the strongest
+    # candidate uses.
+    strongest = candidate_list[0]
+    strongest_translation, (strongest_uses, strongest_message) = strongest
+    use_underscores = "_" in strongest_translation
+    new_candidates = [strongest]
+    for i, candidate in enumerate (candidate_list[1:]):
+        (translation, (uses, message)) = candidate
+        if ((use_underscores
+             and (translation.replace (" ", "_") == strongest_translation))
+            or ((not use_underscores)
+                and (translation.replace ("_", " ") \
+                         == strongest_translation))):
+            # update uses
+            strongest[1][0] += candidate[1][0]
+            sys.stderr.write ((u"Note: %s: ignoring \"%s\" due to underscore normalization.\n" \
+                                  % (input_fn, translation)).encode ("utf-8"))
+            continue
+        new_candidates.append (candidate)
+    candidate_list = new_candidates
+    if len (candidate_list) == 1:
+        return candidate_list
+
+    # Drop all candidates that are essentially unused, compared to the
+    # strongest one.
+    strongest_translation, (strongest_uses, strongest_message) = strongest
+    deleted = 0
+    for i, (_, (uses, _)) in enumerate (candidate_list[1:]):
+        if uses <= strongest_uses / 10:
+            del candidate_list[i+1-deleted]
+            deleted += 1
+    if len (candidate_list) == 1:
+        return candidate_list
+
+    # Case 1: forgotten letter.  If one candidate has minor usage in
+    # comparison to other, and it's missing a single letter, drop it.
+    return candidate_list
+
+added_messages = []
+for msgid, (comment, candidates) in new_messages.iteritems ():
+    candidate_list = filter_candidates (candidates)
+    if len (candidate_list) > 1:
+        sys.stderr.write ("Warning: %s: ambiguous translation of %s.\n" % (input_fn, msgid))
+        for translation, (uses, message) in candidate_list:
+            sys.stderr.write ((u" candidate: %s used %s times\n" % (translation, uses)).encode ("utf-8"))
+    message = candidate_list[0][1][1]
+    message.comment = comment
+    added_messages.append ((msgid, message))
+
+added_messages.sort (key=(lambda (msgid,message):msgid))
+added_messages = list (message for _,message in added_messages)
 po[:0] = added_messages
 print unicode (po).encode ("utf-8")
