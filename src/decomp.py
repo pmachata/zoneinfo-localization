86fc03e37b232ef573f0e3db61998adcf9dc4222
Petr Machata <pmachata@redhat.com>
2010-12-13 19:13:56 +0100

    Only count fuzzy translations if there are same non-fuzzy ones
    - (this doesn't change translations, just cleans up warnings in message
      output)

--- src/decomp.py
+++ src/decomp.py
@@ -11,7 +11,7 @@ input_fn, = sys.argv[1:]
 
 new_messages = {}
 
-def extract_context (tzid, str_comps, top_context, message=None):
+def extract_context (tzid, str_comps, top_context, top_flags, message=None):
     if len (str_comps) != len (tzid.components):
         return
 
@@ -26,19 +26,20 @@ def extract_context (tzid, str_comps, top_context, message=None):
 
         # Pair (use counter, translation)
         candidate = candidates.setdefault (translation,
-                                           [0, polib.POEntry ()])
-        (_, message) = candidate
+                                           [0, 0, polib.POEntry ()])
+        (_, _, message) = candidate
 
         candidates_pair[0] = candidates_pair[0] + " " + top_context.name ()
-        candidate[0] += 1
-        if candidate[0] > 1: # already seen
+        candidate[int (u"fuzzy" in top_flags)] += 1
+        if candidate[0] + candidate[1] > 1: # already seen
             return
 
     message.msgid = tzid.components[-1]
     message.msgstr = translation
     if len (tzid.components) > 1:
         message.msgctxt = "/".join (tzid.components[:-1])
-        extract_context (TZID (message.msgctxt), str_comps[:-1], top_context)
+        extract_context (TZID (message.msgctxt), str_comps[:-1],
+                         top_context, top_flags)
 
 po = polib.pofile (input_fn)
 for message in po:
@@ -48,7 +49,8 @@ for message in po:
         continue
 
     if len (tzid.components) > 1:
-        extract_context (tzid, message.msgstr.split ("/"), tzid, message)
+        extract_context (tzid, message.msgstr.split ("/"), tzid,
+                         message.flags, message)
 
 def filter_candidates (candidates):
     # Here we try to got rid of obvious mistakes.
@@ -56,20 +58,34 @@ def filter_candidates (candidates):
     # Look for the strongest candidate.  Sorting is done by number of
     # uses.  In case of tie, the one without underscores wins.
     candidate_list = list ((list (item) for item in candidates.items ()))
-    def candidate_sorting_key ((translation,(uses,message))):
+    def candidate_sorting_key ((translation,(uses,fuzzy_uses,message))):
         return (uses, "_" not in translation)
     candidate_list.sort (key=candidate_sorting_key, reverse=True)
     if len (candidate_list) == 1:
         return candidate_list
 
+    # Project fuzzy candidate counts if there is evidence that the
+    # translation is correct.
+    for i, candidate in enumerate (candidate_list):
+        (translation, (uses, fuzzy_uses, message)) = candidate
+        if fuzzy_uses > 0:
+            sys.stderr.write ((u"Note: %s: %s %s->%s.\n"
+                               % (input_fn,
+                                  ("not counting purely fuzzy"
+                                   if uses == 0 else "counting fuzzy"),
+                                  message.msgid, translation))
+                              .encode ("utf-8"))
+            if uses != 0:
+                candidate[1][0] += candidate[1][1]
+
     # Normalize use of " " and "_".  Pick whatever the strongest
     # candidate uses.
     strongest = candidate_list[0]
-    strongest_translation, (strongest_uses, strongest_message) = strongest
+    strongest_translation, (strongest_uses, _, strongest_message) = strongest
     use_underscores = "_" in strongest_translation
     new_candidates = [strongest]
     for i, candidate in enumerate (candidate_list[1:]):
-        (translation, (uses, message)) = candidate
+        (translation, (uses, fuzzy_uses, message)) = candidate
         if ((use_underscores
              and (translation.replace (" ", "_") == strongest_translation))
             or ((not use_underscores)
@@ -87,9 +103,9 @@ def filter_candidates (candidates):
 
     # Drop all candidates that are essentially unused, compared to the
     # strongest one.
-    strongest_translation, (strongest_uses, strongest_message) = strongest
+    strongest_translation, (strongest_uses, _, strongest_message) = strongest
     deleted = 0
-    for i, (_, (uses, _)) in enumerate (candidate_list[1:]):
+    for i, (_, (uses, _, _)) in enumerate (candidate_list[1:]):
         if uses <= strongest_uses / 10:
             del candidate_list[i+1-deleted]
             deleted += 1
@@ -105,9 +121,13 @@ for msgid, (comment, candidates) in new_messages.iteritems ():
     candidate_list = filter_candidates (candidates)
     if len (candidate_list) > 1:
         sys.stderr.write ("Warning: %s: ambiguous translation of %s.\n" % (input_fn, msgid))
-        for translation, (uses, message) in candidate_list:
-            sys.stderr.write ((u" candidate: %s used %s times\n" % (translation, uses)).encode ("utf-8"))
-    message = candidate_list[0][1][1]
+        for translation, (uses, fuzzy_uses, message) in candidate_list:
+            sys.stderr.write ((u" candidate: %s used %s times%s\n"
+                               % (translation, uses,
+                                  ((" (%s fuzzy)" % fuzzy_uses)
+                                   if fuzzy_uses != 0 else "")))
+                              .encode ("utf-8"))
+    (_, (_, _, message)) = candidate_list[0]
     message.comment = comment
     added_messages.append ((msgid, message))
 
